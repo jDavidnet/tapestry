6522154155837f85e7bfcf5350b03aa2a314a5d1
Justin Kruger <jdavid.net@gmail.com>
2012-08-18 15:36:41 -0700

    content script now grabs meta and link data

--- chrome/js/content/core.js
+++ chrome/js/content/core.js
@@ -1,6 +1,49 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+console.log('start tapestry');
 
+$(document).ready(function(){
+    var data = {meta:{},links:{}};
+     $('meta[property][content]').each(function(index, element){
+         var $tag = $(element);
+         //console.log('each', arguments);
+         var name = $tag.attr('property');
+         var content = $tag.attr('content')
+         data.meta[name] = content;
+     });
+     $('meta[name][value]').each(function(index, element){
+         var $tag = $(element);
+         //console.log('each', arguments);
+         var name = $tag.attr('name');
+         var content = $tag.attr('value')
+         data.meta[name] = content;
+     });
+     
+     $('meta[name][content]').each(function(index, element){
+         var $tag = $(element);
+         //console.log('each', arguments);
+         var name = $tag.attr('name');
+         var content = $tag.attr('value')
+         data.meta[name] = content;
+     });
+     
+     $('link[rel][href]').each(function(index, element){
+         var $tag = $(element);
+         //console.log('each', arguments);
+         var name = $tag.attr('rel');
+         var content = $tag.attr('href')
+         data.links[name] = content;
+     });
+     
+     console.log('tapestry-meta', data);
+     sendMetaData(data);
+});
 
+function sendMetaData(data, callback, error){
+  chrome.extension.sendMessage({
+    sendMetaData: data
+  }, function(response) {
+    console.log('sendMetaData', response);
+    if(typeof(callback) == 'function'){
+        callback.apply(this, arguments);
+    }
+  });
+};
