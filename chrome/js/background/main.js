8407647f2a090dc03b15e1f972b78ab743b56ee9
Justin Kruger <jdavid.net@gmail.com>
2012-08-18 14:21:18 -0700

    browser_action creates new tab

--- chrome/js/background/main.js
+++ chrome/js/background/main.js
@@ -1,6 +1,20 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+function init(){
+    chrome.browserAction.onClicked.addListener( openTab('html/tapestry.html') );
+}
 
 
+function openTab(url) {
+    console.log('create OpenNewTab Func for ', url);
+    url = chrome.extension.getURL(url)
+    return function (tab){
+        console.log('EVENT click browserAction', tab, arguments);
+        var createProperties = {
+            url:url
+        };
+        chrome.tabs.create(createProperties, function () {
+            console.log('EVENT new browserAction - windowOpen', arguments);
+        });
+    }
+};
+
+init();
