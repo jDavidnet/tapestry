c56cef0353f9e4abf74e8b0c4cf82e1c983be237
Satshabad Khalsa <satshabad.music@gmail.com>
2012-08-18 16:47:56 -0700

    addedimage template

--- chrome/html/tapestry.js
+++ chrome/html/tapestry.js
@@ -1,11 +1,30 @@
-  $(function(){
+chrome.extension.sendMessage({getMetaData: true}, function(response) {
+
+  ogObjects = response.getMetaData;
+
+  var $container = $('#container');
  
-    var $container = $('#container');
- 
-    $container.imagesLoaded( function(){
-      $container.masonry({
-        itemSelector : '.masonryImage'
+  var ogObject = null;
+  
+  
+  console.log(response)
+   
+  console.log(ogObjects.length)
+   
+  for(var i = 0; i<ogObjects.length; i++){
+  
+    ogObject = ogObjects[i];
+
+    $container.append('<div class="ogBrick"><img src="'+ ogObject.meta['og:image']+'" alt="" />	</div>');
+    
+  }    
+
+  $container.imagesLoaded( function(){
+    $container.masonry({
+        itemSelector : '.ogBrick'
       });
-    });
- 
+
   });
+
+
+});
