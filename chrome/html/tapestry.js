8c567ae681933527acaf62381a8d4253c5e26d7f
Satshabad Khalsa <satshabad.music@gmail.com>
2012-08-18 16:48:27 -0700

    now working with basic images

--- chrome/html/tapestry.js
+++ chrome/html/tapestry.js
@@ -7,15 +7,13 @@ chrome.extension.sendMessage({getMetaData: true}, function(response) {
   var ogObject = null;
   
   
-  console.log(response)
    
-  console.log(ogObjects.length)
    
   for(var i = 0; i<ogObjects.length; i++){
   
     ogObject = ogObjects[i];
 
-    $container.append('<div class="ogBrick"><img src="'+ ogObject.meta['og:image']+'" alt="" />	</div>');
+    $container.append('<div class="ogBrick"><img src="'+ ogObject.image_url+'" alt="" />	</div>');
     
   }    
 
