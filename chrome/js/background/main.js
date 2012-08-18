b50a18432a88c5a1cabef36896d504c6b35cc89a
Justin Kruger <jdavid.net@gmail.com>
2012-08-18 15:41:29 -0700

    moved template code to unils.js

--- chrome/js/background/main.js
+++ chrome/js/background/main.js
@@ -16,22 +16,4 @@ function openTab(url) {
     }
 };
 
-/**
- * generates html from a json object and an html template {{varname}}
- * @param template html string
- * @data shallow json object
- */
-function renderTemplate(template, data){
-    var html = '';
-    var name, value;
-    var regex;
-    
-    for(name in data){
-        value = data[name];
-        regex = new RegExp('{{'+name+'}}', 'gi');
-        template = template.replace(regex, value);
-    }
-    return html;
-}
-
 init();
